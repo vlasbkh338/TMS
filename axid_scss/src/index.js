86a0e753c7da7746ec6132cee1139427d61aa66e
vlasbkh338 <vlas338@gmail.com>
2020-10-06 14:07:04 +0300

    adaptive

--- axid_scss/src/index.js
+++ axid_scss/src/index.js
@@ -1,1 +1,36 @@
 import "./scss/main.scss"
+
+const button = document.getElementById("toggle-button")
+const menu = document.getElementById("menu")
+button.addEventListener("click", () => {
+    if (button.classList.contains("active")) {
+        button.classList.remove("active")
+        menu.classList.remove("active")
+        return
+    }
+    button.classList.add("active")
+    menu.classList.add("active")
+})
+
+const clear = (tabsItems, list) => {
+    tabsItems.map((tab, index) => {
+        tab.classList.remove("features__button_active")
+        list[index].classList.remove("features__item_visible")
+    })
+}
+
+const tabs = () => {
+    const tabs = document.getElementById("tabs").children
+    const list = document.getElementById("list").children
+    const tabsItems = Array.prototype.slice.call(tabs, 0)
+
+    tabsItems.map((tab, index) => {
+        tab.addEventListener("click", function() {
+            clear(tabsItems, list)
+            this.classList.add("features__button_active")
+            list[index].classList.add("features__item_visible")
+        })
+    })
+}
+
+tabs()
--- axid_scss/webpack.config.js
+++ axid_scss/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: "cheap-source-map",
     module: {
         rules: [{
                 test: /\.(s*)css$/,
